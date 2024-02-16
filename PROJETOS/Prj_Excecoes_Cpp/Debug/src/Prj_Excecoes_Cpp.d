191f484fa0b478ff7c41ab1d0cbd2a6f10e49333
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-02-16 20:38:18 -0300

    Add files via upload

---
+++ PROJETOS/Prj_Excecoes_Cpp/Debug/src/Prj_Excecoes_Cpp.d
@@ -0,0 +1,1 @@
+src/Prj_Excecoes_Cpp.o: ../src/Prj_Excecoes_Cpp.cpp
---
+++ PROJETOS/Prj_Excecoes_Cpp/Debug/src/subdir.mk
@@ -0,0 +1,31 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/Prj_Excecoes_Cpp.cpp 
+
+CPP_DEPS += \
+./src/Prj_Excecoes_Cpp.d 
+
+OBJS += \
+./src/Prj_Excecoes_Cpp.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp src/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-src
+
+clean-src:
+	-$(RM) ./src/Prj_Excecoes_Cpp.d ./src/Prj_Excecoes_Cpp.o
+
+.PHONY: clean-src
+
