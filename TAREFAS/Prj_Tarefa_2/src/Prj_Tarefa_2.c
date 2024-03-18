1f2c686bff21289e54ad7839373ae093685a5703
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-03-18 11:21:11 -0300

    Update Prj_Tarefa_2.c

--- TAREFAS/Prj_Tarefa_2/src/Prj_Tarefa_2.c
+++ TAREFAS/Prj_Tarefa_2/src/Prj_Tarefa_2.c
@@ -15,36 +15,38 @@
 
 // Função para mostrar os números pares múltiplos de 3 até 1000
 void mostrarNumerosParesMultiplosDeTres() {
+	int cont = 0;
 	printf("Números pares múltiplos de 3 até 1000:\n");
 	for (int i = 2; i <= 1000; i += 2) {
 		if (i % 3 == 0) {
 			printf("%d ", i);
+			cont++;
 		}
 	}
-	printf("\n");
+	printf("\nTotal de números pares múltiplos de 3: %d\n\n", cont);
 }
-
 // Função para ler 10 números inteiros, calcular a soma e a média
 void calcularSomaEMedia() {
-	int numeros[10];
+	int numero;
 	int soma = 0;
 
 	printf("Digite 10 números inteiros:\n");
+	fflush(stdout);
 	for (int i = 0; i < 10; i++) {
-		scanf("%d", &numeros[i]);
-		soma += numeros[i];
+		scanf("%d", &numero);
+		soma += numero;
 	}
 
-	double media = (double) soma / 10;
+	double media = soma / 10;
 
 	printf("Soma: %d\n", soma);
 	printf("Média: %.2lf\n", media);
 }
-
 // Função para calcular a área de 4 cômodos de uma casa
 void calcularAreaDosComodos() {
 	double largura, comprimento, area;
 
+	fflush(stdout);
 	for (int i = 1; i <= 4; i++) {
 		printf("Digite a largura do cômodo %d: ", i);
 		scanf("%lf", &largura);
@@ -57,10 +59,11 @@ void calcularAreaDosComodos() {
 		printf("Área do cômodo %d: %.2lf metros quadrados\n", i, area);
 	}
 }
-
 // Função para calcular o perímetro e a área de um círculo
 void calcularPerimetroEAreaDoCirculo() {
 	double raio, perimetro, area;
+
+	fflush(stdout);
 
 	// Solicitar ao usuário para inserir o valor do raio
 	printf("Digite o valor do raio do círculo: ");
@@ -74,7 +77,6 @@ void calcularPerimetroEAreaDoCirculo() {
 	printf("Perímetro do círculo: %.2lf\n", perimetro);
 	printf("Área do círculo: %.2lf\n", area);
 }
-
 // Função para calcular o valor total das prestações ou à vista
 void calcularPagamento(double totalGasto) {
     int opcaoPagamento;
